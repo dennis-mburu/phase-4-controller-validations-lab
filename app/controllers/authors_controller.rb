4f4ad89b5805042fe992353b006cc28840567f56
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-09-26 11:51:13 +0300

    commit tree c5f44fa50da7fb0103f74c70a38fb32f04e45fef

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,13 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record_response
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
@@ -18,4 +17,7 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+  def render_invalid_record_response(invalid)
+    render json: {errors: invalid.record.errors}, status: 422
+  end
 end
